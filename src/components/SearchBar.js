566921fdad5400d2971f07ab76c121be44075c07
Abuka <maacsgaming@gmail.com>
2019-07-11 14:51:10 +0300

    add form handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  state = { searchTerm: '' }
+
+  onInputChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    // Will add more logic here later
+  }
+
   render () {
     return (
       <div className='search-bar ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={this.handleFormSubmit}>
           <div className='field'>
             <label htmlFor='video'>Video Search </label>
-            <input type='text' name='search' autoComplete='off' placeholder='Search' />
+            <input 
+            type='text' 
+            name='search'
+            autoComplete='off' 
+            placeholder='Search'
+            value={this.state.searchTerm} 
+            onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
